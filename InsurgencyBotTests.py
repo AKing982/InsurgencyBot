6b92d9f27e9eaedddc4e4ed8321186cd559feb04
Alexander King <alex@utahkings.com>
2020-10-25 11:07:16 -0600

    Added pytest to InsurgencyBotTests.py

--- InsurgencyBotTests.py
+++ InsurgencyBotTests.py
@@ -1,20 +1,11 @@
 import unittest
+import pytest
 
-class RedditBotTest(unittest.TestCase):
+class RedditBotTest(pytest.PytestAssertRewriteWarning):
 
     # Test the list
     def testList(self):
         self.list = ['Steyr', 'AUG', 'What do you think ', 'The game is ', 'I like the game ', 'CPU issues ']
-
-        # Test that the length of the list is equal to 6
-        self.assertEqual(len(self.list), 6)
-
-        # Verify that the list contains some of the terms
-        self.assertIn('Steyr', self.list)
-        self.assertIn('The game in', self.list)
-        self.assertIn('CPU', self.list)
-
-        # Verify
 
 
 
