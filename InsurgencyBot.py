5b6d42bea4d11549e67dba5d035fb09a74eb042d
Alexander King <alex@utahkings.com>
2020-10-26 10:25:13 -0600

    Made changes to getHotComments() function and created getUserInfo() function

--- InsurgencyBot.py
+++ InsurgencyBot.py
@@ -5,6 +5,7 @@ from urllib.parse import quote_plus
 
 # Create a list of keywords to search
 key_word_list = ['Steyr', 'AUG', 'What do', 'The game is', 'I like', 'CPU issues ', 'weapons ', 'game issues']
+
 # Create a list of keywords for CPU issues
 cpu_issues_keywords = ['cpu issues', 'cpu performance', 'cpu problems', 'cpu bottleneck']
 
@@ -14,9 +15,6 @@ opinion_keywords = ["What's your thoughts", "How do you think of", "How good is 
                   "how amazing is the game", "opinions", "Is sandstorm worth buying", "worth buying", "issues with game"]
 
 steyr_aug_keywords = ['Steyr', 'AUG', 'steyr', 'aug']
-
-
-
 
 # Create a dictionary to store different categories of comments
 comment_categories = {'FPS Issues: ': [],
@@ -42,26 +40,23 @@ def getHotComments(user_sub_limit, user_question):
                 # If the user_question is about cpu issues get comments relating to cpu issues
                 if user_question in cpu_issues_keywords:
                     print("Submission: ", submission.title)
-                    print("Comments")
+                    print("Comments ", comment.body)
                     print("---------")
-                    print(comment.body)
                     print()
 
                 # Else if the user_question is about fps issues get comments relating to fps issues
                 if user_question in opinion_keywords:
-                    print("Submission: ", submission.title)
-                    print("Comments")
-                    print("---------")
-                    print(comment.body)
-                    print()
+                    # If any of the comments are related to opinions return that comment
+                    if any(opinion in comment_body for opinion in opinion_keywords):
+                        print("Submission: ", submission.title)
+                        print("Comments")
+                        print("---------")
+                        print(comment.body)
+                        print()
 
                 # Else if the user_question is about gpu issues get comments relating to gpu issues
 
                 # Else if the user_question is about opinions of the game get comments relating to opinions
-
-
-
-
 
            # print(comment.body)
             comment_queue.extend(comment.replies)
@@ -82,8 +77,6 @@ def getTopComments(user_sub_limit, user_question):
             for words in steyr_aug_keywords:
                 if words.lower() in comment.body:
                     print(comment.body)
-
-
 
            # print(comment.body)
             comment_queue.extend(comment.replies)
@@ -132,8 +125,6 @@ def getNewComments(user_sub_limit, user_question):
 
     return ''
 
-
-
 def main():
 
     reddit_api = praw.Reddit(client_id='sWoQ6hSvdJueiw',
@@ -142,15 +133,16 @@ def main():
 
     subreddit = reddit_api.subreddit("insurgency")
 
+    getUserInfo()
+
+
+def getUserInfo():
     # Ask the user for the topic, i.e. hot, top, new
     user_topic = input("Enter the topic to be searched: ")
-
     # Ask the user for the number of submissions to be searched
     user_sub_limit = int(input("Enter the desired number of submissions to be searched: "))
-
     # Ask the user what issue they want to search
     user_question = input('Enter the desired issue to be searched: ')
-
     if user_topic == 'hot'.lower():
         print(getHotComments(user_sub_limit, user_question))
     elif user_topic == 'top'.lower():
