54e00c2b8331992bf8e9586c112adad01f164fee
Alexander King <alex@utahkings.com>
2020-11-10 21:15:53 -0700

    Made changes to conditional statement in getHotComments() function

--- InsurgencyBot.py
+++ InsurgencyBot.py
@@ -8,18 +8,19 @@ from urllib.parse import quote_plus
 key_word_list = ['Steyr', 'AUG', 'What do', 'The game is', 'I like', 'CPU issues ', 'weapons ', 'game issues']
 
 
-opinion_keywords = ["What's your thoughts", "How do you think of", "How good is the game",
-                  "how amazing is the game", "opinions", "Is sandstorm worth buying", "worth buying", "issues with game"]
+opinion_keywords = ["opinion", "thoughts", "best about insurgency", "is it fun?", "is it fun", "worth buying?", "worth buying"]
 
 sys_requirements = ["system requirements", "system specs", "pc specs", "PC Specs", "hardware", "memory", "gpu", "cpu",
-                    "motherboard", "RAM", "ram", "i7", "rtx"]
+                    "motherboard", "RAM", "ram", "i7", "rtx", "RTX", "GPU", "specs", "mobo"]
+
+opinion_submissions = ["What are your opinions", ""]
+
 
 def getHotComments():
     reddit_api = praw.Reddit(client_id='sWoQ6hSvdJueiw',
                              client_secret='4JYNsj4ZgzxL1gFSfbjeB2yFMLA',
                              user_agent='<happy:q:1.0>')
     subreddit = reddit_api.subreddit("insurgency")
-
 
     for submission in subreddit.hot(limit=300):
         submission.comments.replace_more(limit=None)
@@ -31,14 +32,24 @@ def getHotComments():
             if len(comment.body) > 5:
                 comment_body = comment.body.lower()
 
-                # For each comment get those that have phrases in sys_requirements
-                if any(question in comment_body for question in sys_requirements):
-                    print("Submission: ", submission.title)
-                    print("Comments")
-                    print("--------")
-                    print(comment.body)
-                    print()
+                # If each comment get those that have phrases in sys_requirements
+                for questions in sys_requirements:
+                    if questions in comment_body:
+                        print("Sys Requirements")
+                        print("Submission: ", submission.title)
+                        print("Comments")
+                        print(comment.body)
+                        print()
+                        break
 
+                # If any comment has phrases in opinion_keywords
+                for questions in opinion_keywords:
+                    if questions in comment_body:
+                        print("Opinions")
+                        print("Submission: ", submission.title)
+                        print("Comments")
+                        print(comment.body)
+                        print()
 
            # print(comment.body)
             comment_queue.extend(comment.replies)
